e8ba6b4cca4ae4dadfd9e76e228cce3284128830
Keerthan Kumar C <ckeerthankumar4@gmail.com>
2024-07-15 11:20:27 +0530

    config changed

--- src/app/db/dbConfig.ts
+++ src/app/db/dbConfig.ts
@@ -4,7 +4,7 @@ dotenvConfig();
 
 export const config = {
   user: process.env.DB_USER || "sa",
-  password: process.env.DB_PASSWORD || "sourabh",
+  password: process.env.DB_PASSWORD || "Kumarc@2004",
   server: process.env.DB_HOST || "localhost",
   database: process.env.DB_NAME || "Route_Delay",
   options: {
